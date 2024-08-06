6b6d80954a6f03030b3c519ccf6989ca1b4f6c30
jazubas <jazbasombrio@gmail.com>
2024-08-06 01:11:36 -0300

    algo base

---
+++ Suscribite.js
@@ -0,0 +1,14 @@
+const codigoAcceso = "#RRR 2024"
+let nombre = prompt ("Decime tu nombre")
+let mailUser = prompt("Ingresá tu mail para acceder a contenidos exclusivos de la web")
+
+if (codigoAcceso == codigoUser){
+    alert("Bienvenidx ${nombre}")
+}else if (codigoAcceso =="#RRR 2023"){
+    alert("${nombre}, por haber participado tenés un acceso VIP en la web")
+}else{
+    alert("No tenés acceso")
+}
+
+
+
