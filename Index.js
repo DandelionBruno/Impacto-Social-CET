0d5e2120403ba3e30a35e8b9f957f9ce20b0d5c6
jazubas <jazbasombrio@gmail.com>
2024-08-05 23:40:00 -0300

    commit -m datos básicos y acceso a la página

---
+++ Index.js
@@ -0,0 +1,15 @@
+//Defino el año de manera constante
+const anio = 2024
+//Pido datos al usuario
+let nombre = prompt("Ingresá tu nombre")
+let apellido = prompt( nombre + "Ingresá tu apellido")
+let anioNacimiento = prompt("¿En qué año naciste?")
+// Calculo la edad del usuario
+let edad = anio - anioNacimiento 
+// Lanzo el mensaje final con toda la información
+alert(nombre + " " + apellido + " según mis cuentas tenés " + edad + " años.")
+
+
+if ((codigoAcceso == codigoUser) || (codigoAcceso =="#RRR 2024")){
+    //Acceso al contenido.
+}
