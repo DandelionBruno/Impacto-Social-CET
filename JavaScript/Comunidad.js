218bcc85130af1aae8d5183397c3ca5c801073a5
jazubas <jazbasombrio@gmail.com>
2024-08-12 02:44:29 -0300

    probando contenido

--- JavaScript/Comunidad.js
+++ JavaScript/Comunidad.js
@@ -3,6 +3,8 @@ const anio = 2024
 //Pido datos al usuario
 let nombre = prompt("Ingresá tu nombre")
 let apellido = prompt( nombre + "Ingresá tu apellido")
+let nombreCompleto = prompt ( nombre + apellido)
+alert( "¡Hola!", nombreCompleto)
 let anioNacimiento = prompt("¿En qué año naciste?")
 // Calculo la edad del usuario
 let edad = anio - anioNacimiento 
--- Suscribite.js
+++ Suscribite.js
@@ -3,14 +3,14 @@ for(let iterador = 0; iterador < 5; iterador++) { let nombre = prompt ("Decime t
 for(let iterador = 0; iterador < 5; iterador++) { let mailUser = prompt("Ingresá tu mail para acceder a contenidos exclusivos de la web") }
 {alert ("Esta es la iteración nro ${i}");}
     if (i == 4)
-    let edad = prompt ("Ingresá tu edad");
+    
+        edad = prompt ("Ingresá tu edad");
     While (edad < 36 || edad > 18)
      {alert("Debés tener entre 18 y 35 años para ser parte de la Comunidad");
     edad = prompt ("Ingresá tu edad");
     }
-// Agregué restricción de edad.
-    {
-break;}
-//Armé el iterador que cuenta la cantidad de veces que repite la acción y cuándo debe detenerse.
+// Agregué restricción de edad y modifiqué errores en el nombre.
+
+
 
 
