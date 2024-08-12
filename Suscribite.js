9ffe419dde900031c242f48ba98aa1149ccd7dd9
jazubas <jazbasombrio@gmail.com>
2024-08-12 02:43:50 -0300

    modificaciones en el nombre y agrego sobre edad

--- Suscribite.js
+++ Suscribite.js
@@ -1,14 +1,16 @@
-const codigoAcceso = "#RRR 2024"
-let nombre = prompt ("Decime tu nombre")
-let mailUser = prompt("Ingresá tu mail para acceder a contenidos exclusivos de la web")
-
-if (codigoAcceso == codigoUser){
-    alert("Bienvenidx ${nombre}")
-}else if (codigoAcceso =="#RRR 2023"){
-    alert("${nombre}, por haber participado tenés un acceso VIP en la web")
-}else{
-    alert("No tenés acceso")
-}
-
+let nombre=""
+for(let iterador = 0; iterador < 5; iterador++) { let nombre = prompt ("Decime tu nombre") ; alert  ("¡Hola!," + nombre) } 
+for(let iterador = 0; iterador < 5; iterador++) { let mailUser = prompt("Ingresá tu mail para acceder a contenidos exclusivos de la web") }
+{alert ("Esta es la iteración nro ${i}");}
+    if (i == 4)
+    let edad = prompt ("Ingresá tu edad");
+    While (edad < 36 || edad > 18)
+     {alert("Debés tener entre 18 y 35 años para ser parte de la Comunidad");
+    edad = prompt ("Ingresá tu edad");
+    }
+// Agregué restricción de edad.
+    {
+break;}
+//Armé el iterador que cuenta la cantidad de veces que repite la acción y cuándo debe detenerse.
 
 
